cdbb4996e17816e3a79ca2c1b9ab9ee45c1180f5
Vivek Shukla <vivekshuklahere@gmail.com>
2023-12-11 19:47:54 +0530

    fix

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 const SignInPage = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-      <SignIn />;
+      <SignIn/>
     </div>
   );
 };
